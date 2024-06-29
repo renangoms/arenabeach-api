c034a3be9a06d221ec833bb47d25cbbf1e5a4af0
renangoms <92700912+renangoms@users.noreply.github.com>
2024-06-29 02:59:23 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*'
+    origin: '*',
+    methods: '*',
+    allowedHeaders: '*',
   });
   
   app.useGlobalPipes(new ValidationPipe());
