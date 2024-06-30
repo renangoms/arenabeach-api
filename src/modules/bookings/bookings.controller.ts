7b9e6e7cde04bb1e6835fc72a65679ba8a18a4a3
renangoms <92700912+renangoms@users.noreply.github.com>
2024-06-30 18:55:16 -0400

    Update bookings.controller.ts

--- src/modules/bookings/bookings.controller.ts
+++ src/modules/bookings/bookings.controller.ts
@@ -50,9 +50,105 @@ export class BookingsController {
     return this.bookingsService.verifyPayment(id, userId);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBookingDto: UpdateBookingDto) {
-    return this.bookingsService.update(+id, updateBookingDto);
+  async createManually(createBookingDto: CreateBookingDto, userId: string) {
+    const { 
+      courtId, 
+      numberOfRackets, 
+      bookingSlots, 
+      userEmail,
+      userPhone,
+      userName,
+      amount,
+     } = createBookingDto;
+
+     const bookingSlotsData = await Promise.all(
+      bookingSlots.map(async booking => {
+        const isReserved = await this.bookingsRepo.findUnique({
+          courtId: courtId,
+          date: TransformDate(booking.bookingDate),
+          startTime: booking.startTime,
+          endTime: booking.endTime,
+          scheduleId: booking.scheduleId,
+        });
+
+        if (isReserved.length > 0) {
+          throw new ConflictException();
+        }
+    
+        return {
+          bookingDate: new Date(booking.bookingDate),
+          startTime: booking.startTime,
+          endTIme: booking.endTime,
+          scheduleId: booking.scheduleId,
+        };
+      })
+    );
+
+    const booking = await this.bookingsRepo.create({
+      data: {
+        courtId,
+        status: 'CONFIRMED',
+        numberOfRackets,
+        userId: userId,
+        bookingSlots: {
+          createMany: {
+            data: bookingSlotsData
+          }
+        },
+        
+      },
+    });
+
+    const chargeBody = {
+      correlationID: randomUUID(),
+      customer: {
+        name: userName,
+        email: userEmail,
+        phone: userPhone,
+      },
+      value: amount,
+      type: 'DYNAMIC',
+      comment: 'Cobrança referente a reserva',
+      expiresIn: Number(env.chargeExpiresIn) ?? 300,
+      additionalInfo: [
+        {
+          key: 'Product',
+          value: 'Reserva'
+        }
+      ]
+    }
+
+    
+    await this.paymentsRepo.create({
+      data: {
+        amount: amount,
+        expiresDate: new Date(),
+        externalChargeId: chargeBody.correlationID,
+        bookingId: booking.id,
+        status: 'CONFIRMED',
+        confirmedAt: booking.createdAt
+      }
+    });
+
+    return {
+      correlationId: chargeBody.correlationID,
+      value: amount,
+      brCode: null,
+      qrCodeImage: null,
+      expiresDate: null,
+      expiresIn: null
+    };
+  }
+
+  async update(id: string, status: 'PENDING' | 'CONFIRMED' | 'CANCELED') {
+    await this.bookingsRepo.update({
+      where: { id: id },
+      data: {
+        status: status
+      }
+    })
+
+    return { ok: true };
   }
 
   @Delete(':id')
