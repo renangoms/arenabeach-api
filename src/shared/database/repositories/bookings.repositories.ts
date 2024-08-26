70d05135d34cc617def2aaeaed928f76561760ea
renangoms <92700912+renangoms@users.noreply.github.com>
2024-08-25 22:37:48 -0400

    Add files via upload

--- src/shared/database/repositories/bookings.repositories.ts
+++ src/shared/database/repositories/bookings.repositories.ts
@@ -70,20 +70,32 @@ export class BookingsRepository {
                 from bookings b
                 inner join booking_slots bs on bs.booking_id = b.id and bs.schedule_id = s.id and bs.booking_date = ${date}
                 inner join payments p on p.booking_id = b.id
-                where b.court_id = c.id and (b.status = 'CONFIRMED' or (p.expires_date > current_timestamp AT TIME ZONE 'UTC' and b.status = 'PENDING'))
+                where b.court_id = c.id and (b.status = 'CONFIRMED' or (p.expires_date > current_timestamp AT TIME ZONE 'UTC' and b.status != 'PENDING'))
             ) as reservado
         from schedules s 
         cross join courts c 
         where s."day_of_week" = ${dayOfWeek}
       )
-      select 
-        "scheduleId",
-        "courtId",
-        "startTime",
-        "endTime",
-        "reservado"
-      from calendary 
-      order by "startTime", "courtId"
+        SELECT 
+            c."scheduleId",
+            c."courtId",
+            c."startTime",
+            c."endTime",
+            c."reservado",
+            (
+                SELECT b.customer_name 
+                FROM booking_slots bs
+                INNER JOIN bookings b ON b.id = bs.booking_id
+                INNER JOIN users u ON u.id = b.user_id
+                WHERE b.status = 'CONFIRMED' 
+                AND bs.start_time = c."startTime" 
+                AND b.court_id = c."courtId"  
+                AND bs.booking_date = ${date}
+                AND bs.schedule_id = c."scheduleId"
+                LIMIT 1 
+            ) AS "customerName"
+        FROM calendary c
+        ORDER BY c."startTime", c."courtId";
 
     `, []);
   }
@@ -102,7 +114,7 @@ export class BookingsRepository {
                 from bookings b
                 inner join booking_slots bs on bs.booking_id = b.id and bs.schedule_id = s.id and bs.booking_date = ${findUniqueDto.date}
                 inner join payments p on p.booking_id = b.id
-                where b.court_id = c.id and (b.status = 'CONFIRMED' or (p.expires_date > current_timestamp AT TIME ZONE 'UTC' and b.status = 'PENDING'))
+                where b.court_id = c.id and (b.status = 'CONFIRMED' or p.expires_date > current_timestamp AT TIME ZONE 'UTC')
             ) as reservado
         from schedules s 
         cross join courts c 
